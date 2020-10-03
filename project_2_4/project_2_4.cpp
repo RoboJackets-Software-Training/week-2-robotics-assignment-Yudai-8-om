0ce8d8e0dc6c5381b012fe34c6492f5387aafefa
vagrant <noah.bjyu@gmail.com>
2020-10-03 16:26:40 +0000

    completed

--- project_2_4/project_2_4.cpp
+++ project_2_4/project_2_4.cpp
@@ -17,22 +17,93 @@ struct LightInputs {
 };
 
 // TODO Define your light controller state machine class here
+class LightController{
+public:
+  LightOutputs update(const LightInputs &inputs){
+    checkForTransitions(inputs);
+    return runState();
 
-int main()
-{
+  }
+
+  void checkForTransitions(const LightInputs &inputs){
+    switch (current_state_){
+      case RED_ALL:
+        if (inputs.car_sensor_n || inputs.car_sensor_s){
+          current_state_ = GREEN_NS;
+        } else {
+          current_state_ = GREEN_EW;
+        } 
+        next_transition_time_ = inputs.time + 5;
+        break;
+      case YELLOW_NS:
+        if (next_transition_time_ <= inputs.time){
+          current_state_ = RED_ALL;
+        }
+        break;
+      case GREEN_NS:
+        if (next_transition_time_ <= inputs.time){
+          current_state_ = YELLOW_NS;
+          next_transition_time_ = inputs.time + 2;
+        }
+        break;
+      case YELLOW_EW:
+      if (next_transition_time_ <= inputs.time){
+          current_state_ = RED_ALL;
+        }
+        break;
+      case GREEN_EW:
+        if (next_transition_time_ <= inputs.time){
+          current_state_ = YELLOW_EW;
+          next_transition_time_ = inputs.time + 2;
+        }
+        break;
+    }
+  }
+
+  LightOutputs runState(){
+    switch (current_state_){
+      case RED_ALL:
+        return {true, false, false, true, false, false};
+      case YELLOW_NS:
+        return {false, true, false, true, false, false};
+      case GREEN_NS:
+        return {false, false, true, true, false, false};
+      case YELLOW_EW:
+        return {true, false, false, false, true, false};
+      case GREEN_EW:
+        return {true, false, false, false, false, true};
+    }
+  }
+
+private:
+  enum State {RED_ALL, YELLOW_NS, GREEN_NS, YELLOW_EW, GREEN_EW};
+  State current_state_{RED_ALL};
+  int next_transition_time_ {0};
+};
+
+
+int main(){
+  int time = 0;
+  LightController controller;
     // TODO Initialize your state machine class here
-    while(true)
-    {
+    while(true){
         std::string input;
         std::getline(std::cin, input);
         if(input.size() != 2)
             break;
 
         // TODO Parse the input digits
-
+          const LightInputs inputs{time, input[0] == '1', input[1] == '1'};
+        
         // TODO Update your state machine based on input
+        const LightOutputs output = controller.update(inputs);
 
         // TODO Output your state machine's light controls to std::cout
-    }
+        std::cout << output.red_ns << output.yellow_ns << output.green_ns << 
+                     output.red_ew << output.yellow_ns << output.green_ew << std::endl;
+        time++;
+      }
+    
+
     return 0;
 }
